486f3d01243c71b24b7dff6ec1d494a27a01883e
Idan Kober <idan.kober@gmail.com>
2020-03-07 19:42:08 +0200

    saving skipped images

--- session.js
+++ session.js
@@ -3,9 +3,13 @@ var sessionDiv = document.getElementById("session");
 var slideshow_image = document.getElementById("slideshow_image");
 var actionProgressBar;
 var currentTimeoutId;
+var currentImageId;
+var currentAction;
+var skippedImages = [];
 
 function startSession() {
     saveSettingsToLocalStorage();
+    initSkippedImages();
     actionsQueue = createQueueFromSessionJSON();
 
     startASessionDiv.style.display = "none";
@@ -13,10 +17,17 @@ function startSession() {
 
     actionProgressBar = new ProgressBar.Circle('#action-progress-bar', {
         strokeWidth: 2,
-        color: '#FFFFFF'
+        color: '#FFFFFF',
+        warnings: true
     });
 
     nextAction();
+}
+
+function initSkippedImages() {
+  if (localStorage.getItem("skippedImages")) {
+    skippedImages = JSON.parse(localStorage.getItem("skippedImages"));
+  }
 }
 
 function saveSettingsToLocalStorage() {
@@ -24,47 +35,50 @@ function saveSettingsToLocalStorage() {
 }
 
 function createQueueFromSessionJSON() {
-    let session = editor.get().session;
-    let queue = [];
+  let session = editor.get().session;
+  let queue = [];
 
-    session.forEach(section => {
-      if (section.type = "slideshow") {
-        for (let index = 0; index < section.images_to_show; index++) {
-          queue.push({
-            type: "image",
-            time: section.time_per_image
-          });
-          queue.push({
-            type: "break",
-            text: "get ready for next image",
-            time: section.time_between_images
-          });
-        }
+  session.forEach(section => {
+    if (section.type = "slideshow") {
+      for (let index = 0; index < section.images_to_show; index++) {
+        queue.push({
+          type: "image",
+          time: section.time_per_image
+        });
+        queue.push({
+          type: "break",
+          text: "get ready for next image",
+          time: section.time_between_images
+        });
       }
-    });
+    }
+  });
 
-    return queue;
-  }
+  return queue;
+}
 
 function nextAction() {
   if (actionsQueue.length > 0) {
-      const nextAction = actionsQueue.shift();
+      currentAction = actionsQueue.shift();
 
-      if (nextAction.type == "image") {
-          showImage(nextAction);
-      } else if (nextAction.type == "break") {
-          breakTime(nextAction);
+      if (currentAction.type == "image") {
+          showImage(currentAction);
+      } else if (currentAction.type == "break") {
+          breakTime(currentAction);
       }
   }
 }
 
 function showImage(imageConfiguration) {
-  const imageId = files[Math.floor(Math.random() * files.length)].id;
+  currentImageId = files[Math.floor(Math.random() * files.length)].id;
+  while (skippedImages.includes(currentImageId)) {
+    currentImageId = files[Math.floor(Math.random() * files.length)].id;
+  }
   
-  slideshow_image.src = "https://drive.google.com/uc?id=" + imageId;
+  slideshow_image.src = "https://drive.google.com/uc?id=" + currentImageId;
+  actionProgressBar.set(0);
   slideshow_image.onload = function () {
-      actionProgressBar.set(0);
-      actionProgressBar.animate(1, {duration: imageConfiguration.time * 1000});
+      actionProgressBar.animate(1, {duration: imageConfiguration.time * 1000 - 1000});
       currentTimeoutId = setTimeout(nextAction, imageConfiguration.time * 1000);
   };
 }
@@ -72,10 +86,17 @@ function showImage(imageConfiguration) {
 function breakTime(breakConfiguration) {
   slideshow_image.src = "";
   actionProgressBar.set(0);
-  currentTimeoutId = setTimeout(nextAction, breakConfiguration.time * 1000);
+  currentTimeoutId = setTimeout(nextAction, breakConfiguration.time * 1000 - 1000);
 }
 
 function skipAction() {
+  if (currentAction.type == "image") {
+    skippedImages.push(currentImageId);
+    localStorage.setItem("skippedImages", JSON.stringify(skippedImages));
+  }
+
+  actionProgressBar.stop();
   clearTimeout(currentTimeoutId);
+  actionsQueue.push(currentAction);
   nextAction();
 }
